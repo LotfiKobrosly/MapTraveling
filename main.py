206aae0d45cd794bd93c25dc0c8352423c393d52
Lotfi Kobrosly <lotfi@pcinv20231212>
2026-02-16 13:06:20 +0100

    Updating main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import os
 import warnings
 import random
 import numpy as np
+import pandas as pd
 
 from utils.sampling_utils import *
 from utils.constants import *
@@ -16,20 +18,66 @@ if __name__ == "__main__":
     height_bounds = [100, 300]
     width_bounds = [100, 300]
     trajectory_max_length = 400
+    n_maps = 10
+    n_runs = 10
+    inputs = {"level": 1}
 
-    current_map = generate_random_map_with_rectangular_obstacles(
-        100, 100, n_obstacles_max
-    )
-    start_point, goal = generate_start_and_end_points(current_map)
-    inputs = {
-        "n_iterations": 100,
-        "level": 1,
-    }
-    for strategy in strategies:
-        path_generator = PathGenerator(
-            current_map, start_point, goal, trajectory_max_length, strategy
+    # Figures saving main directory
+    figures_directory = "./figures"
+    if not os.path.exists(figures_directory):
+        os.mkdir(figures_directory)
+
+    # Saving scores
+    mean_score = np.zeros((n_maps, len(strategies)))
+    std_score = np.zeros((n_maps, len(strategies)))
+    min_score = np.zeros((n_maps, len(strategies)))
+
+    for map_id in range(n_mapsœ):
+        print("\nRun n°", int(map_id), ":...")
+
+        # Figures for map
+        map_figures_directory = figures_directory + "/" + "map_" + int(map_id)
+        if not os.path.exists(map_figures_directory):
+            os.mkdir(map_figures_directory)
+        current_map = generate_random_map_with_rectangular_obstacles(
+            100, 100, n_obstacles_max
         )
-        path_generator.run(inputs)
-        score = path_generator.get_score()
-        play_scenario(path_generator.get_movement_frames(), strategy.upper(), score)
-        print(strategy, " run done")
+        start_point, goal = generate_start_and_end_points(current_map)
+        
+        for strategy_id, strategy in enumerate(strategies):
+            # Strategy per map saving directory
+            strategy_map_directory = map_figures_directory + "/" + strategy.upper()
+            scores_list = list()
+            if not os.path.exists(strategy_map_directory):
+                os.mkdir(strategy_map_directory)
+
+            # Running solver
+            for run in range(n_runs):
+                path_generator = PathGenerator(
+                    current_map, start_point, goal, trajectory_max_length, strategy
+                )
+                if strategy == "random_walk":
+                    inputs["n_iterations"] = 1e4
+                else:
+                    inputs["n_iterations"] = 100
+                path_generator.run(inputs)
+                score = path_generator.get_score()
+                scores_list.append(score)
+                play_scenario(path_generator.get_movement_frames(), strategy_map_directory + "/run_" + int(run), score)
+            mean_score[map_id, strategy_id] = np.mean(scores_list)
+            std_score[map_id, strategy_id] = np.std(scores_list)
+            min_score[map_id, strategy_id] = np.min(scores_list)
+            print(strategy.upper(), " runs done")
+
+    writer = pd.ExcelWriter("Aggregated_scores.xlsx", engine="xlsxwriter")
+
+    mean_dataframe = pd.DataFrame(data=mean_score, columns=strategies, index=range(1, n_maps + 1))
+    std_dataframe = pd.DataFrame(data=std_score, columns=strategies, index=range(1, n_maps + 1))
+    min_dataframe = pd.DataFrame(data=min_score, columns=strategies, index=range(1, n_maps + 1))
+
+    mean_dataframe.to_excel(writer, sheet_name="Mean score")
+    std_dataframe.to_excel(writer, sheet_name="Standard deviation of score")
+    min_dataframe.to_excel(writer, sheet_name="Min score")
+
+    writer.close()
+
