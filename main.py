a0b33595a4cfed5423d7b2445248bb5ab4820897
Lotfi Kobrosly <lotfi@pcinv20231212>
2026-05-07 21:08:42 +0200

    Change main.py to run discrete baselines

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import time
 import warnings
 import random
+import json
 import numpy as np
 import pandas as pd
 
@@ -19,25 +20,61 @@ if __name__ == "__main__":
         "MCTS": {"strategy": "mcts", "inputs": {"n_iterations": 10000}},
         "RAVE": {"strategy": "rave", "inputs": {"n_iterations": 10000}},
         "GRAVE": {"strategy": "grave", "inputs": {"n_iterations": 10000}},
-        "cMCTS": {"strategy": "cmcts", "inputs": {"n_iterations": 10000}},
-        "cRAVE": {"strategy": "crave", "inputs": {"n_iterations": 10000}},
-        "cGRAVE": {"strategy": "cgrave", "inputs": {"n_iterations": 10000}},
-        "cNMCTS_level_1": {"strategy": "cnmcts", "inputs": {"level": 1, "bandwidth": 25}},
-        "cRbNMCTS_level_1": {"strategy": "crbnmcts", "inputs": {"level": 1, "bandwidth": 25}},
-        "cNMCTS_level_2": {"strategy": "cnmcts", "inputs": {"level": 2, "bandwidth": 10}},
-        "cRbNMCTS_level_2": {"strategy": "crbnmcts", "inputs": {"level": 2, "bandwidth": 15}},
-        "cNRPA_level_1": {"strategy": "nrpa", "inputs": {"level": 1, "n_policies": 200}},
-        "cPbRNRPA_level_1": {"strategy": "pbrnrpa", "inputs": {"level": 1, "n_policies": 200}},
-        "cGNRPA_level_1": {"strategy": "gnrpa", "inputs": {"level": 1, "n_policies": 200}},
-        "cABGNRPA_level_1": {"strategy": "abgnrpa", "inputs": {"level": 1, "n_policies": 200}},
-        "cNRPA_level_2": {"strategy": "nrpa", "inputs": {"level": 2, "n_policies": 50}},
-        "cPbRNRPA_level_2": {"strategy": "pbrnrpa", "inputs": {"level": 2, "n_policies": 50}},
-        "cGNRPA_level_2": {"strategy": "gnrpa", "inputs": {"level": 2, "n_policies": 50}},
-        "cABGNRPA_level_2": {"strategy": "abgnrpa", "inputs": {"level": 2, "n_policies": 50}},
+        # "cMCTS": {"strategy": "cmcts", "inputs": {"n_iterations": 10000}},
+        # "cRAVE": {"strategy": "crave", "inputs": {"n_iterations": 10000}},
+        # "cGRAVE": {"strategy": "cgrave", "inputs": {"n_iterations": 10000}},
+        # "cNMCTS_level_1": {
+        #     "strategy": "cnmcts",
+        #     "inputs": {"level": 1, "bandwidth": 25},
+        # },
+        # "cRbNMCTS_level_1": {
+        #     "strategy": "crbnmcts",
+        #     "inputs": {"level": 1, "bandwidth": 25},
+        # },
+        # "cNMCTS_level_2": {
+        #     "strategy": "cnmcts",
+        #     "inputs": {"level": 2, "bandwidth": 10},
+        # },
+        # "cRbNMCTS_level_2": {
+        #     "strategy": "crbnmcts",
+        #     "inputs": {"level": 2, "bandwidth": 15},
+        # },
+        # "cNRPA_level_1": { 
+        #     "strategy": "nrpa",
+        #     "inputs": {"level": 1, "n_policies": 200},
+        # },
+        # "cPbRNRPA_level_1": {
+        #     "strategy": "pbrnrpa",
+        #     "inputs": {"level": 1, "n_policies": 200},
+        # },
+        # "cGNRPA_level_1": {
+        #     "strategy": "gnrpa",
+        #     "inputs": {"level": 1, "n_policies": 200},
+        # },
+        # "cABGNRPA_level_1": {
+        #     "strategy": "abgnrpa",
+        #     "inputs": {"level": 1, "n_policies": 200},
+        # },
+        # "cNRPA_level_2": {"strategy": "nrpa", "inputs": {"level": 2, "n_policies": 50}},
+        # "cPbRNRPA_level_2": {
+        #     "strategy": "pbrnrpa",
+        #     "inputs": {"level": 2, "n_policies": 50},
+        # },
+        # "cGNRPA_level_2": {
+        #     "strategy": "gnrpa",
+        #     "inputs": {"level": 2, "n_policies": 50},
+        # },
+        # "cABGNRPA_level_2": {
+        #     "strategy": "abgnrpa",
+        #     "inputs": {"level": 2, "n_policies": 50},
+        # },
     }
+    
     n_obstacles_max = 10
     trajectory_max_length = 100
-    n_maps = 10
+    maps_file = "./difficult_maps/difficult_maps.json"
+    start_map_id = 0
+    n_maps = 15
     n_runs = 10
 
     # Figures saving main directory
@@ -46,26 +83,21 @@ if __name__ == "__main__":
         os.mkdir(figures_directory)
 
     # Saving scores
-    mean_score = np.zeros((n_maps, len(strategies)))
-    std_score = np.zeros((n_maps, len(strategies)))
-    min_score = np.zeros((n_maps, len(strategies)))
-    average_time = np.zeros((n_maps, len(strategies)))
+    mean_score = np.zeros((start_map_id + n_maps, len(strategies)))
+    std_score = np.zeros((start_map_id + n_maps, len(strategies)))
+    min_score = np.zeros((start_map_id + n_maps, len(strategies)))
+    average_time = np.zeros((start_map_id + n_maps, len(strategies)))
 
-    for map_id in range(n_maps):
+    for map_id in range(start_map_id, start_map_id + n_maps):
         print("\nMap n°", str(map_id + 1))
 
         # Figures for map
-        map_figures_directory = figures_directory + "/" + "map_" + str(map_id)
+        map_figures_directory = figures_directory + "/" + "map_" + str(map_id + 1)
         if not os.path.exists(map_figures_directory):
             os.mkdir(map_figures_directory)
 
-        # Map dimensions
-        height = 100
-        width = 100
-        current_map = generate_random_map_with_rectangular_obstacles(
-            height, width, n_obstacles_max
-        )
-        start_point, goal = generate_start_and_end_points(current_map)
+        # Get the map
+        current_map, start_point, goal = load_raw_map(maps_file, str(map_id + 1))
 
         for strategy_id, strategy in enumerate(strategies.keys()):
             # Strategy per map saving directory
@@ -82,7 +114,11 @@ if __name__ == "__main__":
                     print("Run n°: ", run + 1)
                     start_time = time.time()
                     path_generator = PathGenerator(
-                        current_map, start_point, goal, trajectory_max_length, strategies[strategy]["strategy"]
+                        current_map,
+                        start_point,
+                        goal,
+                        trajectory_max_length,
+                        strategies[strategy]["strategy"],
                     )
                     run_solver(path_generator, strategies[strategy]["inputs"])
                     score = path_generator.best_score
@@ -93,15 +129,16 @@ if __name__ == "__main__":
                         strategy_map_directory + "/run_" + str(run),
                         score,
                     )
-                except KeyboardInterrupt:
-                    continue
-            mean_score[map_id, strategy_id] = np.mean(scores_list)
-            std_score[map_id, strategy_id] = np.std(scores_list)
+                except Exception:
+                    scores_list.append(np.nan)
+                    time_list.append(np.nan)
+            mean_score[map_id, strategy_id] = np.nanmean(scores_list)
+            std_score[map_id, strategy_id] = np.nanstd(scores_list)
             min_score[map_id, strategy_id] = np.min(scores_list)
-            average_time[map_id, strategy_id] = np.mean(time_list)
+            average_time[map_id, strategy_id] = np.nanmean(time_list)
             print(strategy, " runs done")
 
-    writer = pd.ExcelWriter("Aggregated_scores.xlsx", engine="xlsxwriter")
+    writer = pd.ExcelWriter("Aggregated_scores_discrete.xlsx", engine="xlsxwriter")
 
     mean_dataframe = pd.DataFrame(
         data=mean_score, columns=strategies, index=range(1, n_maps + 1)
